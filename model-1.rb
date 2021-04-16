142b99ba385f4ca6aa228c9f628cc7a68551068e
Lee Hopper <hopper.lee.j@gmail.com>
2021-04-15 19:56:14 -0600

    Add variables tht use floats/integers

--- model-1.rb
+++ model-1.rb
@@ -3,4 +3,9 @@
 # Variables that use strings
 location = "Vail"
 weekday = "Saturday"
-conditions = "Powder"
+conditions = "Snowing"
+
+# Variables that use floats/integers
+inches_of_fresh_snow = 10
+lifts_open = 20
+reservations = 20000
